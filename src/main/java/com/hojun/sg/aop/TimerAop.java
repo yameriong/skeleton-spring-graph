1f2536d67a2bb1bae345a4ab637430d34eace991
Hojun Choi <hojunchoi@hotmail.co.kr>
2023-04-12 00:00:18 +0900

    Add JPA StopWatch
    Todo benchmark Test
    1. Add query conditional statement
    2. Stream combine filters

---
+++ src/main/java/com/hojun/sg/aop/TimerAop.java
@@ -0,0 +1,29 @@
+package com.hojun.sg.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
+
+@Aspect
+@Component
+public class TimerAop {
+    @Pointcut("@annotation(Timer)")
+    private void enableTimer() {}
+
+    @Around("enableTimer()")
+    public Object around(ProceedingJoinPoint joinPoint) throws Throwable {
+        var stopWatch = new StopWatch();
+        stopWatch.start();
+
+        Object result = joinPoint.proceed();
+
+        stopWatch.stop();
+
+        System.out.println("total time is : " + stopWatch.getTotalTimeMillis());
+
+        return result;
+    }
+}
--- src/main/java/com/hojun/sg/bus/application/MyBusService.java
+++ src/main/java/com/hojun/sg/bus/application/MyBusService.java
@@ -1,17 +1,23 @@
 package com.hojun.sg.bus.application;
 
+import com.hojun.sg.aop.Timer;
 import com.hojun.sg.bus.domain.MyBusStationInfo;
 import com.hojun.sg.bus.domain.MyBusStationInfos;
 import com.hojun.sg.bus.mock.MockDataLoader;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
 public class MyBusService {
+    @Timer
+    @Transactional(readOnly = true)
     public MyBusStationInfos getMyBusStationInfoByAllUser() {
         return MockDataLoader.ALL_USER_BUS_INFO;
     }
+    @Timer
+    @Transactional(readOnly = true)
     public MyBusStationInfo getMyBusStationInfoByPersonId(String id) {
         return MockDataLoader.ALL_USER_BUS_INFO.hasInfo(id);
     }
